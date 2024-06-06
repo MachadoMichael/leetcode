e90bace3d83146efa0eb2dc623a8c0d507609738
Michael Machado <michael.machado@microware.com.br>
2024-06-06 17:27:16 -0300

    feat: mergeSortedArrays.go

--- q4/mergeSortedArrays.go
+++ q4/mergeSortedArrays.go
@@ -3,10 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	arr1 := []int{1, 2, 3, 14}
+	arr2 := []int{5, 6, 17, 18}
+	fmt.Printf("%v ", mergeSortedArrays(arr1, arr2))
 }
 
 func mergeSortedArrays(arr1, arr2 []int) []int {
+	var result []int
+	i, j := 0, 0
 
-	return arr1
+	for i < len(arr1) && j < len(arr2) {
+		if arr1[i] <= arr2[j] {
+			result = append(result, arr1[i])
+			i++
+		} else {
+			result = append(result, arr2[j])
+			j++
+		}
+
+	}
+
+	for i < len(arr1) {
+		result = append(result, arr1[i])
+		i++
+	}
+
+	for j < len(arr2) {
+		result = append(result, arr2[j])
+		j++
+	}
+
+	return result
 }
