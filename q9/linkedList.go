dc5fec38f0b5b36fb28d56238138f52f43f56d68
Michael <machado.m3@icloud.com>
2024-06-19 09:02:06 -0300

    refactor: linkedList methods

--- q9/linkedList.go
+++ q9/linkedList.go
@@ -1,42 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
 
 type LinkedList struct {
 	Head   *LinkedItem
-	Lenght uint
+	Lenght int
 }
 
 type LinkedItem struct {
-	Node int
-	Next *LinkedItem
+	Value int
+	Next  *LinkedItem
 }
 
 func (l *LinkedList) append(node *LinkedItem) *LinkedList {
-	if l.Head.Next == nil {
-		l.Head.Next = node
+	var currentlyNode *LinkedItem = l.Head
+	if currentlyNode == nil {
+		l.Head = node
 		l.Lenght++
 		return l
-	} else {
-		return l.append(node)
 	}
+
+	for currentlyNode.Next != nil {
+		currentlyNode = currentlyNode.Next
+	}
+
+	if currentlyNode.Next == nil {
+		currentlyNode.Next = node
+		l.Lenght++
+	}
+
+	return l
 }
 
 func (l *LinkedList) prepend(node *LinkedItem) *LinkedList {
 	node.Next = l.Head
 	l.Head = node
+	l.Lenght++
 	return l
 }
 
 func (l *LinkedList) insert(node *LinkedItem, index int) *LinkedList {
+	if l.Lenght < index || index < 0 {
+		log.Fatalln("index out range.")
+	}
+
+	if l.Lenght == 0 {
+		l.Head = node
+		l.Lenght++
+		return l
+	}
+
 	i := 0
-	var selectedNode *LinkedItem
+	var selectedNode *LinkedItem = l.Head
 	var beforeNode *LinkedItem
 	for i < index {
 		if i == index-1 {
 			beforeNode = selectedNode
 		}
-		selectedNode = findNode(l.Head)
+		selectedNode = selectedNode.Next
+		i++
 	}
 
 	beforeNode.Next = node
@@ -45,23 +69,14 @@ func (l *LinkedList) insert(node *LinkedItem, index int) *LinkedList {
 	return l
 }
 
-func (l *LinkedList) getNode(index int) *LinkedItem {
-	var selectedNode *LinkedItem
-	i := 0
-	for i < index-1 {
-		selectedNode = findNode(l.Head)
-		i++
+func printNodes(node *LinkedItem) {
+	println(node.Value)
+	if node.Next != nil {
+		printNodes(node.Next)
 	}
-
-	return selectedNode
-}
-
-func findNode(node *LinkedItem) *LinkedItem {
-	return node.Next
 }
 
 func main() {
-	fmt.Println("vim-go")
 
 	linked := LinkedList{
 		Head:   nil,
@@ -69,21 +84,29 @@ func main() {
 	}
 
 	tail2 := LinkedItem{
-		Node: 12,
-		Next: nil,
+		Value: 12,
+		Next:  nil,
 	}
 
 	tail3 := LinkedItem{
-		Node: 99,
-		Next: nil,
+		Value: 99,
+		Next:  nil,
 	}
 
-	fmt.Println(linked.Head)
-	linked.prepend(&tail2)
-	linked.prepend(&tail2)
-	linked.prepend(&tail2)
-	linked.prepend(&tail2)
-	linked.prepend(&tail2)
-	linked.insert(&tail3, 2)
-	fmt.Println(linked.getNode(2))
+	tail4 := LinkedItem{
+		Value: 55,
+		Next:  nil,
+	}
+
+	tail5 := LinkedItem{
+		Value: 77,
+		Next:  nil,
+	}
+
+	linked.append(&tail3)
+	linked.append(&tail4)
+	linked.insert(&tail2, 1)
+	linked.prepend(&tail5)
+	printNodes(linked.Head)
+	println("Lenght: ", linked.Lenght)
 }
